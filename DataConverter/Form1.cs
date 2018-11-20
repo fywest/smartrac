cfff14c0a025c2ae00334a733a4569a6c03ca137
fywest <fywest2109@hotmail.com>
2018-11-20 14:13:32 +0200

    add ReadCsv file

--- DataConverter/Form1.cs
+++ DataConverter/Form1.cs
@@ -26,19 +26,24 @@ namespace DataConverter
 {
     public partial class Form1 : Form
     {
-        private const int MaxColumns = 64;
-        private int MaxIDPerFile;
-        private int TotalID;
-        protected string FileName;
-        protected bool Modified;
+        public const int MaxColumns = 64;
+        public static int MaxIDPerFile;
+        public static int TotalID;
+
+        public static string FileName;
+        public static bool Modified;
+
         public static InitFile Ini;
         public static SWTEncoding swt;
+
+        public static ReadCsv csvFile;
         
 
         public Form1()
         {
             InitializeComponent();
             Ini = new InitFile(InitFile.IniPath);
+            csvFile = new ReadCsv();
             swt = new SWTEncoding();
            
         }
@@ -46,7 +51,7 @@ namespace DataConverter
         private void Form1_Load(object sender, EventArgs e)
         {
             //InitializeGrid();
-            ClearFile();
+            csvFile.ClearFile();
         }
 
         private void ReadConfiguration()
@@ -83,12 +88,7 @@ namespace DataConverter
         /// </summary>
 
 
-        private void ClearFile()
-        {
-            //dataGridView1.Rows.Clear();
-            FileName = null;
-            Modified = false;
-        }
+
 
         //private bool ReadIniFile(string filename)
         //{
@@ -107,183 +107,7 @@ namespace DataConverter
         //    return false;
         //}
 
-        private bool ReadFile(string filename)
-        {
-            Cursor = Cursors.WaitCursor;
-            string[] outputFile;
-            int fileNum;
-            string refFile = "";
-            int row_total = 1, row_current = 1;
-            int ID_count;
-            if (TotalID > 0)
-            {
-                ID_count = TotalID; 
-            }
-            else ID_count = File.ReadAllLines(filename).Length;
-
-            fileNum = ID_count / MaxIDPerFile;
-            if (ID_count % MaxIDPerFile != 0) fileNum++;
-
-            outputFile = new string[fileNum];
-            if (filename.EndsWith(".csv"))
-            {
-                
-
-                if (ID_count >= MaxIDPerFile)
-                {
-                    for (int i = 0; i < fileNum; i++)
-                    {
-                        outputFile[i] = filename.Substring(0, filename.LastIndexOf(".csv"))
-                            + "_SWT__" + (i + 1).ToString() + "-" + fileNum.ToString() + "__NotStarted.csv";
-                        if (File.Exists(outputFile[i]))
-                        {
-                            File.Delete(outputFile[i]);
-                        }
-                        using (StreamWriter sw = File.CreateText(outputFile[i]))
-                        {
-                            sw.WriteLine(SWTEncoding.WriteHeader());
-                            sw.Close();
-
-                        }
-                    }
-                }
-                else {
-                    //outputFile = new string[1];
-                    outputFile[0] = filename.Substring(0, filename.LastIndexOf(".csv")) + "_SWT__1-1__NotStarted.csv";
-                    if (File.Exists(outputFile[0]))
-                    {
-                        File.Delete(outputFile[0]);
-                    }
-                }
-                //only for lowcase in file ext name
-                refFile = filename.Substring(0, filename.LastIndexOf(".csv")) + "_REF.csv";
-
-                //refFile = "REF.csv";
-            }
-
-         //   outputFile = "SWT_" + filename;
-  /*          if (File.Exists(outputFile))
-            {
-                File.Delete(outputFile);
-            }*/
-            if (File.Exists(refFile))
-            {
-                File.Delete(refFile);
-            }
-            try
-            {
-                //dataGridView1.Rows.Clear();
-                List<string> columns = new List<string>();
-                int cnt = 0;
-                
-                using (var reader = new CsvFileReader(filename))
-                {
-                    if(SWTEncoding.in_rows < 0) // dynamic encoding from csv
-                    {
-                        while (reader.ReadRow(columns))
-                        {
-                            //dataGridView1.Rows.Add(columns.ToArray());
-                            // data converting here
-                            // Create a file to write to. 
-                            //using (StreamWriter sw = File.CreateText(outputFile[cnt]))
-                            if (!File.Exists(outputFile[cnt]))
-                            {
-                                // Create a file to write to. 
-                                using (StreamWriter sw = File.CreateText(outputFile[cnt]))
-                                {
-                                    sw.WriteLine(SWTEncoding.WriteHeader());
-                                }
-                            }
-                            
-                            using (StreamWriter sw = File.AppendText(outputFile[cnt]))
-                            {
-                                //sw.WriteLine(SWTEncoding.WriteHeader());
-                                //if (row_total >= SWTEncoding.in_start_row)
-                                //{
-
-                                    
-                                    sw.WriteLine(SWTEncoding.Convert(columns[SWTEncoding.cColumnToParse], refFile));
-                                    //}
-                                    row_total++;
-                                    row_current++;
-                                    if (row_current > MaxIDPerFile)
-                                    {
-
-                                        cnt++;
-                                        row_current = 1;
-                                        SWTEncoding.out_index = 0;
-                                    }
-
-                                if (!SWTEncoding.cParserFirstLineParsed)
-                                {
-                                    if (SWTEncoding.SearchForURL(columns) >= 0)
-                                    {
-                                        // parse first row here
-                                        TB_staticURL.Text = columns[SWTEncoding.cColumnToParse];
-                                        SWTEncoding.cParserFirstLineParsed = true;
-                                        TB_commands.Text = FEIG.cmd1 + "\r\n" + FEIG.cmd2;
-                                    }
-                                    else continue;
-                                }
-
-                            }
-                            
-                            /*
-                            else
-                            {
-                                File.Delete(outputFile[cnt]);
-                                
-                                using (StreamWriter sw = File.AppendText(outputFile[cnt]))
-                                {
-                                    sw.WriteLine(SWTEncoding.Convert(columns, refFile));
-                                }
-                                
-                            }*/
-                        }
-                    }
-                    else // static encoding => no use of HMAC, only generate commands
-                    {
-                        
-                            // Create a file to write to. 
-                            
-                                while (row_total <= TotalID)
-                                {
-                            using (StreamWriter sw = File.AppendText(outputFile[cnt]))
-                            {
-                                sw.WriteLine(SWTEncoding.Convert("", refFile));
-                                row_current++;
-                                row_total++;
-                                if (row_current > MaxIDPerFile)
-                                    {
-
-                                        cnt++;
-                                        row_current = 1;
-                                        SWTEncoding.out_index = 0;
-                                    }
-                                
-
-                            }
-
-                        }
-                        
-                    }
-                    
-                    
-                }
-                FileName = filename;
-                Modified = false;
-                return true;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(String.Format("Error reading from {0}.\r\n\r\n{1}", filename, ex.Message));
-            }
-            finally
-            {
-                Cursor = Cursors.Default;
-            }
-            return false;
-        }
+       
 
         private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
@@ -296,7 +120,10 @@ namespace DataConverter
             if (SWTEncoding.cUrlType == SWTEncoding.UrlType.DynamicURL)
             {
                 //SWTEncoding.ParseURL();
-                ReadFile(FileName);
+                
+                csvFile.ReadFile(FileName);
+                TB_staticURL.Text = csvFile.str_staticURL;
+                TB_commands.Text = csvFile.str_commands;
             }
             else if (SWTEncoding.cUrlType == SWTEncoding.UrlType.StaticURL)
             {
@@ -308,7 +135,7 @@ namespace DataConverter
         private void RandomGen_Click(object sender, EventArgs e)
         {
             ReadConfiguration();
-            ReadFile("SLIX2_destroy_password.csv");
+            csvFile.ReadFile("SLIX2_destroy_password.csv");
         }
     }
 }
---
+++ DataConverter/ReadCsv.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvFile;
+
+namespace DataConverter
+{
+    public class ReadCsv
+    {
+
+        public string str_staticURL;
+        public string str_commands;
+
+        public void ClearFile()
+        {
+            //dataGridView1.Rows.Clear();
+            Form1.FileName = null;
+            Form1.Modified = false;
+        }
+
+        public bool ReadFile(string filename)
+        {
+            //lidev
+            //Cursor = Cursors.WaitCursor;
+            string[] outputFile;
+            int fileNum;
+            string refFile = "";
+            int row_total = 1, row_current = 1;
+            int ID_count;
+            if (Form1.TotalID > 0)
+            {
+                ID_count = Form1.TotalID;
+            }
+            else ID_count = File.ReadAllLines(filename).Length;
+
+            fileNum = ID_count / Form1.MaxIDPerFile;
+            if (ID_count % Form1.MaxIDPerFile != 0) fileNum++;
+
+            outputFile = new string[fileNum];
+            if (filename.EndsWith(".csv"))
+            {
+
+
+                if (ID_count >= Form1.MaxIDPerFile)
+                {
+                    for (int i = 0; i < fileNum; i++)
+                    {
+                        outputFile[i] = filename.Substring(0, filename.LastIndexOf(".csv"))
+                            + "_SWT__" + (i + 1).ToString() + "-" + fileNum.ToString() + "__NotStarted.csv";
+                        if (File.Exists(outputFile[i]))
+                        {
+                            File.Delete(outputFile[i]);
+                        }
+                        using (StreamWriter sw = File.CreateText(outputFile[i]))
+                        {
+                            sw.WriteLine(SWTEncoding.WriteHeader());
+                            sw.Close();
+
+                        }
+                    }
+                }
+                else
+                {
+                    //outputFile = new string[1];
+                    outputFile[0] = filename.Substring(0, filename.LastIndexOf(".csv")) + "_SWT__1-1__NotStarted.csv";
+                    if (File.Exists(outputFile[0]))
+                    {
+                        File.Delete(outputFile[0]);
+                    }
+                }
+                //only for lowcase in file ext name
+                refFile = filename.Substring(0, filename.LastIndexOf(".csv")) + "_REF.csv";
+
+                //refFile = "REF.csv";
+            }
+
+            //   outputFile = "SWT_" + filename;
+            /*          if (File.Exists(outputFile))
+                      {
+                          File.Delete(outputFile);
+                      }*/
+            if (File.Exists(refFile))
+            {
+                File.Delete(refFile);
+            }
+            try
+            {
+                //dataGridView1.Rows.Clear();
+                List<string> columns = new List<string>();
+                int cnt = 0;
+
+                using (var reader = new CsvFileReader(filename))
+                {
+                    if (SWTEncoding.in_rows < 0) // dynamic encoding from csv
+                    {
+                        while (reader.ReadRow(columns))
+                        {
+                            //dataGridView1.Rows.Add(columns.ToArray());
+                            // data converting here
+                            // Create a file to write to. 
+                            //using (StreamWriter sw = File.CreateText(outputFile[cnt]))
+                            if (!File.Exists(outputFile[cnt]))
+                            {
+                                // Create a file to write to. 
+                                using (StreamWriter sw = File.CreateText(outputFile[cnt]))
+                                {
+                                    sw.WriteLine(SWTEncoding.WriteHeader());
+                                }
+                            }
+
+                            using (StreamWriter sw = File.AppendText(outputFile[cnt]))
+                            {
+                                //sw.WriteLine(SWTEncoding.WriteHeader());
+                                //if (row_total >= SWTEncoding.in_start_row)
+                                //{
+
+
+                                sw.WriteLine(SWTEncoding.Convert(columns[SWTEncoding.cColumnToParse], refFile));
+                                //}
+                                row_total++;
+                                row_current++;
+                                if (row_current > Form1.MaxIDPerFile)
+                                {
+
+                                    cnt++;
+                                    row_current = 1;
+                                    SWTEncoding.out_index = 0;
+                                }
+
+                                if (!SWTEncoding.cParserFirstLineParsed)
+                                {
+                                    if (SWTEncoding.SearchForURL(columns) >= 0)
+                                    {
+                                        // parse first row here
+                                        //TB_staticURL.Text = columns[SWTEncoding.cColumnToParse];
+                                        str_staticURL = columns[SWTEncoding.cColumnToParse];
+                                        SWTEncoding.cParserFirstLineParsed = true;
+                                        //TB_commands.Text = FEIG.cmd1 + "\r\n" + FEIG.cmd2;
+                                        str_commands = FEIG.cmd1 + "\r\n" + FEIG.cmd2;
+                                    }
+                                    else continue;
+                                }
+
+                            }
+
+                            /*
+                            else
+                            {
+                                File.Delete(outputFile[cnt]);
+                                
+                                using (StreamWriter sw = File.AppendText(outputFile[cnt]))
+                                {
+                                    sw.WriteLine(SWTEncoding.Convert(columns, refFile));
+                                }
+                                
+                            }*/
+                        }
+                    }
+                    else // static encoding => no use of HMAC, only generate commands
+                    {
+
+                        // Create a file to write to. 
+
+                        while (row_total <= Form1.TotalID)
+                        {
+                            using (StreamWriter sw = File.AppendText(outputFile[cnt]))
+                            {
+                                sw.WriteLine(SWTEncoding.Convert("", refFile));
+                                row_current++;
+                                row_total++;
+                                if (row_current > Form1.MaxIDPerFile)
+                                {
+
+                                    cnt++;
+                                    row_current = 1;
+                                    SWTEncoding.out_index = 0;
+                                }
+
+
+                            }
+
+                        }
+
+                    }
+
+
+                }
+                Form1.FileName = filename;
+                Form1.Modified = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Error reading from {0}.\r\n\r\n{1}", filename, ex.Message));
+            }
+            finally
+            {
+                //lidev
+                //Cursor = Cursors.Default;
+            }
+            return false;
+        }
+    }
+}
