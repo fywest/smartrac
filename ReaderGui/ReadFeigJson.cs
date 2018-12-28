387c34e863aa30b77a85daf7c0fc934f1fd455d9
fywest <fywest2109@hotmail.com>
2018-12-28 02:09:04 +0200

    add feig.json which can build readfeigjson object

--- ReaderGui/ReadFeigJson.cs
+++ ReaderGui/ReadFeigJson.cs
@@ -11,8 +11,8 @@ namespace ReaderGui
 {
     public struct CommandJson
     {
-        public string icName;
-        public string[] content;
+        public string[] icName;
+        public string[] icCommand;
     }
 
     public class Student
@@ -32,11 +32,18 @@ namespace ReaderGui
 
     public class FeigJson
     {
-        public string ReadManufacturer;
-        public string model;
-        public string[] SupportedProtocols;
-        public List<Command> SupportedICs;
-       
+        //public string ReadManufacturer;
+        public string Model { get; set; }
+        public string[] SupportedProtocols { get; set; }
+        public string[] SupportedICs { get; set; }
+        public List<CommandJson> SetupCommands { get; set; }
+
+    }
+    public class FeigJsonList
+    {
+        public string ReaderManufacturer;
+        public string[] SupportedModels;
+        public List<FeigJson> ReaderConfig;
     }
 
     class ReadFeigJson
@@ -48,46 +55,86 @@ namespace ReaderGui
 
         public string showInfo()
         {
-            string json = @"{ 'Name':'C#','Age':'3000','ID':'1','Sex':'女'}";
-            
-            Student descJsonStu = JsonConvert.DeserializeObject<Student>(json);//反序列化
-            string info=(string.Format("反序列化： ID={0},Name={1},Sex={2},Sex={3}", descJsonStu.ID, descJsonStu.Name, descJsonStu.Age, descJsonStu.Sex));
-         
 
-            string jsonfile = "HF_reader_FEIG2_configuration_ver2_1.json";
 
-            using (System.IO.StreamReader file = System.IO.File.OpenText(jsonfile))
-            {
-                using (JsonTextReader reader = new JsonTextReader(file))
-                {
-                    JObject o = (JObject)JToken.ReadFrom(reader);
-                    string ReadManufacturer = o["ReadManufacturer"].ToString();
-                    var SupportedModels = o["SupportedModels"];
-                    //var c = b["lotaddress"];
-                    //var d = o["devices"];
-                    foreach (JObject item in SupportedModels)
-                    {
-                        string model = item["model"].ToString();
-                        var config = item["config"];
-                        foreach (JObject item1 in config)
-                        {
-                            var SupportedProtocols = item1["SupportedProtocols"].ToString();
-                            var SupportedICs = item1["SupportedICs"].ToString();
-                            var SetupCommands = item1["SetupCommands"].ToString();
-                            foreach (JObject item11 in SetupCommands)
-                            {
-                                string name = item11["name"].ToString();
-                                string content = item11["content"].ToString();
-                            }
 
-                        }
 
-                    }
-                }
-            }
 
-            return info;
+            //string jsonFile = "feigPR40.json";
+            //FeigJson feigjson = JsonConvert.DeserializeObject<FeigJson>(File.ReadAllText(jsonFile));
+
+            string jsonFile = "feig.json";
+            FeigJsonList feigjson = JsonConvert.DeserializeObject<FeigJsonList>(File.ReadAllText(jsonFile));
+
+
+            return "info";
         }
 
     }
 }
+
+//string json = @"{ 'Name':'C#','Age':'3000','ID':'1','Sex':'女'}";
+
+//Student descJsonStu = JsonConvert.DeserializeObject<Student>(json);//反序列化
+//string info=(string.Format("反序列化： ID={0},Name={1},Sex={2},Sex={3}", descJsonStu.ID, descJsonStu.Name, descJsonStu.Age, descJsonStu.Sex));
+
+
+//"HF_reader_FEIG2_configuration_ver2_1.json";
+//   string jsonStr = @"{
+//	'model': 'PR40',
+//	'SupportedProtocols': [
+//		'ISO14443A',
+//		'ISO14443B',
+//		'ISO14443A-4',
+//		'SRI',
+//		'Mifare Classic',
+//		'MyD',
+//		'ISO15693'
+//	],
+//	'SupportedICs': [
+//		'TAG213',
+//		'NTAG210',
+//		'SIC43NT'
+//	],
+//	'SetupCommands': [
+//		{
+//			'icName': ['NTAG213','NTAG210','SIC43NT'],
+//			'icCommand': [
+//				'YYYY'
+//			]
+//		}
+//	]
+//}";
+//FeigJson feigjson = JsonConvert.DeserializeObject<FeigJson>(jsonStr);
+
+
+
+//using (System.IO.StreamReader file = System.IO.File.OpenText(jsonfile))
+//{
+//    using (JsonTextReader reader = new JsonTextReader(file))
+//    {
+//        JObject o = (JObject)JToken.ReadFrom(reader);
+//        string ReadManufacturer = o["ReadManufacturer"].ToString();
+//        var SupportedModels = o["SupportedModels"];
+//        //var c = b["lotaddress"];
+//        //var d = o["devices"];
+//        foreach (JObject item in SupportedModels)
+//        {
+//            string model = item["model"].ToString();
+//            var config = item["config"];
+//            foreach (JObject item1 in config)
+//            {
+//                var SupportedProtocols = item1["SupportedProtocols"].ToString();
+//                var SupportedICs = item1["SupportedICs"].ToString();
+//                var SetupCommands = item1["SetupCommands"].ToString();
+//                foreach (JObject item11 in SetupCommands)
+//                {
+//                    string name = item11["name"].ToString();
+//                    string content = item11["content"].ToString();
+//                }
+
+//            }
+
+//        }
+//    }
+//}
